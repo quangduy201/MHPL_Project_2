f067e4681e8176f637e7f5e2bd971b49d280ba4e
BTH-IT <hungbienthanh@gmail.com>
2024-03-28 00:18:31 +0700

    change database

---
+++ src/main/java/com/example/project_2/DTO/ThanhVien.java
@@ -0,0 +1,81 @@
+package com.example.project_2.DTO;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hung
+ */
+@Entity
+@Table(name = "thanhvien")
+public class ThanhVien {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int MaTV;
+
+    @Column(name = "HoTen", nullable = false)
+    private String HoTen;
+
+    @Column(name = "Khoa", length = 100)
+    private String Khoa;
+
+    @Column(name = "Nganh", length = 100)
+    private String Nganh;
+
+    @Column(name = "SDT")
+    private int SDT;
+
+    public int getMaTV() {
+        return MaTV;
+    }
+
+    public void setMaTV(int MaTV) {
+        this.MaTV = MaTV;
+    }
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    public String getKhoa() {
+        return Khoa;
+    }
+
+    public void setKhoa(String Khoa) {
+        this.Khoa = Khoa;
+    }
+
+    public String getNganh() {
+        return Nganh;
+    }
+
+    public void setNganh(String Nganh) {
+        this.Nganh = Nganh;
+    }
+
+    public int getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(int SDT) {
+        this.SDT = SDT;
+    }
+
+    
+    
+}
---
+++ src/main/java/com/example/project_2/DTO/XuLy.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.project_2.DTO;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Hung
+ */
+@Entity
+@Table(name = "xuly")
+public class XuLy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int MaXL;
+
+    @ManyToOne
+    @JoinColumn(name = "MaTV")
+    private ThanhVien thanhVien;
+
+    @Column(name = "HinhThucXL", length = 250)
+    private String HinhThucXL;
+
+    @Column(name = "SoTien")
+    private int SoTien;
+
+    @Column(name = "NgayXL")
+    private LocalDateTime NgayXL;
+
+    @Column(name = "TrangThaiXL")
+    private int TrangThaiXL;
+
+    public XuLy(int MaXL, ThanhVien thanhVien, String HinhThucXL, int SoTien, LocalDateTime NgayXL, int TrangThaiXL) {
+        this.MaXL = MaXL;
+        this.thanhVien = thanhVien;
+        this.HinhThucXL = HinhThucXL;
+        this.SoTien = SoTien;
+        this.NgayXL = NgayXL;
+        this.TrangThaiXL = TrangThaiXL;
+    }
+
+    public int getMaXL() {
+        return MaXL;
+    }
+
+    public void setMaXL(int MaXL) {
+        this.MaXL = MaXL;
+    }
+
+    public ThanhVien getThanhVien() {
+        return thanhVien;
+    }
+
+    public void setThanhVien(ThanhVien thanhVien) {
+        this.thanhVien = thanhVien;
+    }
+
+    public String getHinhThucXL() {
+        return HinhThucXL;
+    }
+
+    public void setHinhThucXL(String HinhThucXL) {
+        this.HinhThucXL = HinhThucXL;
+    }
+
+    public int getSoTien() {
+        return SoTien;
+    }
+
+    public void setSoTien(int SoTien) {
+        this.SoTien = SoTien;
+    }
+
+    public LocalDateTime getNgayXL() {
+        return NgayXL;
+    }
+
+    public void setNgayXL(LocalDateTime NgayXL) {
+        this.NgayXL = NgayXL;
+    }
+
+    public int getTrangThaiXL() {
+        return TrangThaiXL;
+    }
+
+    public void setTrangThaiXL(int TrangThaiXL) {
+        this.TrangThaiXL = TrangThaiXL;
+    }
+
+    
+}
