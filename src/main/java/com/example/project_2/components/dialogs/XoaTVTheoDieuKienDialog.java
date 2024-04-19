e91970129812eb7778ddb13782e222cbb77e4b4f
YnVy <nguyenyenvy266@gmail.com>
2024-04-19 18:59:42 +0700

    form XoaTVTheoDieuKien

---
+++ src/main/java/com/example/project_2/components/dialogs/XoaTVTheoDieuKienDialog.java
@@ -0,0 +1,269 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package com.example.project_2.components.dialogs;
+
+import java.awt.Color;
+import javax.swing.table.DefaultTableModel;
+import org.jdesktop.animation.timing.Animator;
+import org.jdesktop.animation.timing.TimingTarget;
+import org.jdesktop.animation.timing.TimingTargetAdapter;
+import com.example.project_2.BLL.ThietBiBLL;
+import com.example.project_2.DTO.ThietBi;
+import com.example.project_2.GUI.Main;
+
+/**
+ *
+ * @author Admin
+ */
+public class XoaTVTheoDieuKienDialog extends javax.swing.JDialog {
+    private int DEFALUT_WIDTH;
+    private DefaultTableModel model;
+    
+    private ThietBiBLL thietbiBLL = new ThietBiBLL();
+
+    public boolean isOk() {
+        return ok;
+    }
+
+    public void setOk(boolean ok) {
+        this.ok = ok;
+    }
+
+    private boolean ok;
+    private final Animator animator;
+    private boolean show = true;
+    
+    public XoaTVTheoDieuKienDialog(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+            
+        setOpacity(0f);
+        getContentPane().setBackground(Color.WHITE);
+        TimingTarget target = new TimingTargetAdapter() {
+            @Override
+            public void timingEvent(float fraction) {
+                if (show) {
+                    setOpacity(fraction);
+                } else {
+                    setOpacity(1f - fraction);
+                }
+            }
+
+            @Override
+            public void end() {
+                if (show == false) {
+                    setVisible(false);
+                }
+            }
+
+        };
+        animator = new Animator(200, target);
+        animator.setResolution(0);
+        animator.setAcceleration(0.5f);
+        
+        setLocationRelativeTo(null);
+        
+    }
+    
+    public void xoaNhieuThietBi(){
+        if (CheckValid()){
+            String matb = txtNam.getText();
+
+            if (thietbiBLL.deleteAllById(matb)){
+                Message mess = new Message(Main.getFrames()[0], true);
+                mess.showMessage("Xóa nhiều thiết bị theo mã thành công");
+                setOk(true);
+                dispose();
+            } else{
+                Message mess = new Message(Main.getFrames()[0], true);
+                mess.showMessage("Xóa nhiều thiết bị theo mã thất bại");
+                setOk(false);
+            }
+        }
+    }
+    
+    public void showDialog() {
+        animator.start();
+        setVisible(true);
+    }
+    
+    private void closeMenu() {
+        if (animator.isRunning()) {
+            animator.stop();
+        }
+        show = false;
+        animator.start();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        buttonGroup4 = new javax.swing.ButtonGroup();
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        txtNam = new com.example.project_2.components.swing.TextField();
+        btnHuy = new com.example.project_2.components.swing.Button();
+        btnDelete = new com.example.project_2.components.swing.Button();
+
+        setUndecorated(true);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 255)));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 102, 255)));
+
+        jLabel1.setFont(new java.awt.Font("sansserif", 1, 34)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(159, 159, 159));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Xóa TV theo điều kiện");
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel6.setText("Năm nhập học:");
+
+        txtNam.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNamActionPerformed(evt);
+            }
+        });
+
+        btnHuy.setText("Hủy");
+        btnHuy.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnHuyActionPerformed(evt);
+            }
+        });
+
+        btnDelete.setBackground(new java.awt.Color(51, 102, 255));
+        btnDelete.setForeground(new java.awt.Color(255, 255, 255));
+        btnDelete.setText("Xóa");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtNam, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 30, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(62, 62, 62))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(13, 13, 13)
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(txtNam, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(26, Short.MAX_VALUE))
+        );
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 250));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btCancelActionPerformed
+       
+    }//GEN-LAST:event_btCancelActionPerformed
+
+    private void btAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btAddActionPerformed
+    
+    }//GEN-LAST:event_btAddActionPerformed
+  
+    private void btResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btResetActionPerformed
+    
+    }//GEN-LAST:event_btResetActionPerformed
+
+    private void txtTimeKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtTimeKeyTyped
+
+    }//GEN-LAST:event_txtTimeKeyTyped
+
+    private void txtNamActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNamActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtNamActionPerformed
+
+    private void btnHuyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHuyActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnHuyActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        xoaNhieuThietBi();
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
+    private boolean CheckValid() {
+        if (txtNam.getText().isBlank()) {
+            Message mess = new Message(Main.getFrames()[0], true);
+            mess.showMessage("Không được để trống mã thiết bị");
+            return false;
+        }
+        if (txtNam.getText().trim().length() != 7) {
+            Message mess = new Message(Main.getFrames()[0], true);
+            mess.showMessage("Mã thiết bị phải có 7 số");
+            return false;
+        }
+        if (!txtNam.getText().matches("\\d+")) {
+            Message mess = new Message(Main.getFrames()[0], true);
+            mess.showMessage("Mã thiết bị không được chứa ký tự");
+            return false;
+        }
+        if (thietbiBLL.getById(Integer.parseInt(txtNam.getText())) != null) {
+            Message mess = new Message(Main.getFrames()[0], true);
+            mess.showMessage("Mã thiết bị đã tồn tại");
+            return false;
+        }
+        
+        return true;
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.example.project_2.components.swing.Button btnDelete;
+    private com.example.project_2.components.swing.Button btnHuy;
+    private javax.swing.ButtonGroup buttonGroup4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JProgressBar jProgressBar1;
+    private com.example.project_2.components.swing.TextField txtNam;
+    // End of variables declaration//GEN-END:variables
+
+}
